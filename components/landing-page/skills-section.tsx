1cbcc18b35c46805b6d74ec5578d0ea205390c7d
meetmakwana7396 <makwnameet7301@gmail.com>
2024-07-14 23:55:27 +0530

    added skill section

--- components/landing-page/skills-section.tsx
+++ components/landing-page/skills-section.tsx
@@ -2,9 +2,21 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { Badge } from "../ui/badge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { TypeIcon } from "lucide-react";
 import { faSquareJs } from "@fortawesome/free-brands-svg-icons/faSquareJs";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
+import {
+  faBootstrap,
+  faNode,
+  faNodeJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import IconTypescript from "../icons/icon-typescript";
+import IconNextjs from "../icons/icon-nextjs";
+import IconTailwind from "../icons/icon-tailwind";
+import IconRedux from "../icons/icon-redux";
+import IconRadix from "../icons/icon-radix";
+import IconExpress from "../icons/icon-express";
+import IconAdonis from "../icons/icon-adonis";
+import IconSql from "../icons/icon-sql";
 
 export default function SkillsSection() {
   return (
@@ -13,9 +25,9 @@ export default function SkillsSection() {
       <div className="grid grid-cols-2 gap-4">
         <SkillCategoryCard className="col-span-2">
           <h2 className="text-xl text-green-300"># Frontend</h2>
-          <div className="flex gap-4 items-center">
+          <div className="flex flex-wrap gap-4 items-center">
             <SkillBadge>
-              <TypeIcon className="mr-2 size-5" />
+              <IconTypescript className="mr-2 size-[18px]" />
               Typescript
             </SkillBadge>
             <SkillBadge>
@@ -25,16 +37,54 @@ export default function SkillsSection() {
             <SkillBadge>
               <FontAwesomeIcon icon={faReact} className="size-5 mr-2" /> React
             </SkillBadge>
-            <SkillBadge>Next</SkillBadge>
+            <SkillBadge>
+              <IconNextjs className="mr-2 size-[18px]" />
+              Next
+            </SkillBadge>
+            <SkillBadge>
+              <IconRadix className="mr-2 size-[18px]" />
+              Radix UI
+            </SkillBadge>
+            <SkillBadge>
+              <IconTailwind className="mr-2 size-[18px] " />
+              Tailwind
+            </SkillBadge>
+            <SkillBadge>
+              <FontAwesomeIcon icon={faBootstrap} className="mr-2 size-5" />
+              Bootstrap
+            </SkillBadge>
+            <SkillBadge>
+              <IconRedux className="mr-2 size-[18px]" />
+              Redux
+            </SkillBadge>
           </div>
         </SkillCategoryCard>
         <SkillCategoryCard>
           <h2 className="text-xl text-green-300"># Backend</h2>
-          <div>Node, Express, Adonis</div>
+          <div className="flex flex-wrap gap-4 items-center">
+            <SkillBadge>
+              <FontAwesomeIcon icon={faNodeJs} className="mr-2 size-5" />
+              Node
+            </SkillBadge>
+            <SkillBadge>
+              <IconExpress className="mr-2 size-[18px]" />
+              Express
+            </SkillBadge>
+            <SkillBadge>
+              <IconAdonis className="mr-2 size-[18px]" />
+              Adonis
+            </SkillBadge>
+          </div>
         </SkillCategoryCard>
         <SkillCategoryCard>
           <h2 className="text-xl text-green-300"># Database</h2>
-          <div>SQL, NoSQL, MySQL, MongoDB, Postgres</div>
+          <div className="flex flex-wrap gap-4 items-center">
+            <SkillBadge>SQL</SkillBadge>
+            <SkillBadge>NoSQL</SkillBadge>
+            <SkillBadge>MySQL</SkillBadge>
+            <SkillBadge>MongoDB</SkillBadge>
+            <SkillBadge>Postgres</SkillBadge>
+          </div>
         </SkillCategoryCard>
       </div>
     </section>
