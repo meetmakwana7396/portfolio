74f9cff3e74d81ca600bac95a87a9350d274ac2a
meetmakwana7396 <makwnameet7301@gmail.com>
2024-08-02 23:08:19 +0530

    image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.hashnode.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.hashnode.com",
+      },
+    ],
   },
 };
 
