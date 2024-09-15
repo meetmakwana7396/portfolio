9ffe0e98ea97d23d045990ba43611ecb4e26bb79
meetmakwana7396 <makwnameet7301@gmail.com>
2024-09-15 21:25:12 +0530

    sitemap changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_HASHNODE_APP_URL}`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_HASHNODE_APP_URL}/articles`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/articles`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
